b7038a77e49503fb3347d46b2ec935eafe63eb12
Ali <alifeyza4017@gmail.com>
2022-09-12 13:30:42 +0300

    added

--- js.exercise/script.js
+++ js.exercise/script.js
@@ -13,13 +13,23 @@
 //  document.querySelector(".guess").value = 23
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1
+
+console.log(secretNumber)
+document.querySelector(".number").textContent = secretNumber
+
 document.querySelector(".check").addEventListener("click", function() {
-    const a = Number(document.querySelector('.guess').value);
-    console.log(a, typeof a)
+    const guess= Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess)
 
-    if(!a) {
-    document.querySelector(".message").textContent = "noo";
-
+    if(!guess) {
+    document.querySelector(".message").textContent = "😢 No number";
+    } else if (guess === secretNumber) {
+        document.querySelector(".message").innerText = "😍 Correct Number";
+    }else if (guess>secretNumber) {
+        document.querySelector(".message").textContent = "🅰Too high"
+    }else if (guess<secretNumber) {
+        document.querySelector(".message").innerText = "💥Too lower"
     }
- 
+    
 })
