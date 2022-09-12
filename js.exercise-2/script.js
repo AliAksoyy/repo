66bd4d1791cd843b9d121500d41d5752064cfce4
Ali <alifeyza4017@gmail.com>
2022-09-12 16:28:07 +0300

    added

--- js.exercise-2/script.js
+++ js.exercise-2/script.js
@@ -4,8 +4,12 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector("overlay");
 const btnCloseModal = document.querySelector(".close-modal")
 const btnsOpenModal = document.querySelectorAll(".show-modal")
-console.log(btnsOpenModal)
+console.log(btnsOpenModal);
 
-for(let i in btnsOpenModal) {
-    console.log(btnsOpenModal[i].textContent)
-};
+for(let i = 0; i<btnsOpenModal.lenght; i++) 
+    btnsOpenModal[i].addEventListener("click", function () {
+        console.log("button clicked");
+        modal.classList.remove("hidden");
+        overlay.classList.remove("hidden");
+    });
+
