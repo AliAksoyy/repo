a1a311ca3a9e3aefe1a0da8bbc4c3c01d0c8bd64
Ali <alifeyza4017@gmail.com>
2022-09-12 12:14:20 +0300

    added

--- js.exercise/script.js
+++ js.exercise/script.js
@@ -1,1 +1,14 @@
 'use strict';
+console.log(document.querySelector(".message").innerText)
+
+document.querySelector(".message").textContent = "😍 Correct Number"
+
+console.log(document.querySelector(".message").innerText)
+
+let h1 = document.querySelector(".number").textContent = 13
+document.querySelector(".score").innerText = 10
+
+
+
+ document.querySelector(".guess").value = 23
+console.log(document.querySelector('.guess').value);
