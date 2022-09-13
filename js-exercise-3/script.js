945e21554c4c5583afa90f9168175ffd01b3a1bf
Ali <alifeyza4017@gmail.com>
2022-09-13 06:42:49 +0300

    added

--- js-exercise-3/script.js
+++ js-exercise-3/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+
+const diceEl = document.querySelector(".dice")
+
+
+score0El.textContent = 0;
+score1El.innerText = 0;
+diceEl.classList.add("hidden")
+
+
