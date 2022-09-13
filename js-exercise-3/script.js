63eeaf4fe72ae9e1387bd791a3a585a3c343b037
Ali <alifeyza4017@gmail.com>
2022-09-13 08:06:11 +0300

    added

--- js-exercise-3/script.js
+++ js-exercise-3/script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector(".player--0")
+const player1El = document.querySelector(".player--1")
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 
@@ -34,6 +36,8 @@ btnRoll.addEventListener("click", function() {
         document.getElementById(`current--${activePlayer}`).textContent = 0
         activePlayer = activePlayer === 0 ? 1 : 0
         currentScore = 0
+        player0El.classList.toggle("player--active")
+        player1El.classList.toggle("player--active")
     }
 
 
