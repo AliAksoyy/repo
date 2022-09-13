0528368eddf8a86b4081da051832eabaed521408
Ali <alifeyza4017@gmail.com>
2022-09-13 15:38:34 +0300

    added

--- js-exercise-3/script.js
+++ js-exercise-3/script.js
@@ -13,17 +13,26 @@ const btnHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0")
 const current1El = document.getElementById("current--1")
 
+let scores, currentScore, activePlayer, playing;
 
+const init = function () {
+     scores = [0, 0];
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
 
+     score0El.textContent = 0;
+     score1El.textContent = 0;
+     current0El.textContent = 0;
+     current1El.textContent = 0;
+     diceEl.classList.add('hidden');
+     player0El.classList.remove("player--winner")
+     player1El.classList.remove("player--winner")
+     player0El.classList.add("player--active")
+     player1El.classList.remove("player--active")
 
-score0El.textContent = 0;
-score1El.innerText = 0;
-diceEl.classList.add("hidden")
-
-const scores = [0,0]
-let currentScore = 0
-let activePlayer = 0;
-let playing = true;
+}
+init()
 
 const swtichPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -73,5 +82,7 @@ btnHold.addEventListener("click", function () {
       }
     }
 })
+
+btnNew.addEventListener("click", init) 
 
 
