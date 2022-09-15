1425ed03065a7138cb8d9266cda061acae8280fc
Ali <alifeyza4017@gmail.com>
2022-09-15 09:30:07 +0300

    added

--- js.exercise/script.js
+++ js.exercise/script.js
@@ -1,6 +1,4 @@
-console.log( document.querySelector(".message").textContent)
-document.querySelector(".message").textContent = "Corret Number"
+// 
 
-document.querySelector(".number").textContent = 10
 
-document.querySelector(".guess").value= 23
+
