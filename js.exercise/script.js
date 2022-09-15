7f1f6adbb2f73d74e421032a4cf568790264160c
Ali <alifeyza4017@gmail.com>
2022-09-15 10:49:04 +0300

    added

--- js.exercise/script.js
+++ js.exercise/script.js
@@ -5,9 +5,10 @@ const check = document.querySelector(".check")
 
 const message = document.querySelector(".message")
 let score = 20
-const secretNumber =Math.trunc( Math.random() *20 +1)
-// console.log(secretNumber)
-document.querySelector(".number").textContent = secretNumber
+let highScore = 0
+let secretNumber =Math.trunc( Math.random() *20 +1)
+console.log(secretNumber)
+
 check.addEventListener("click", () => {
 const guess = Number(document.querySelector('.guess').value);
   if(!guess) {
@@ -16,6 +17,12 @@ const guess = Number(document.querySelector('.guess').value);
     document.querySelector('.message').textContent = 'Corret number';
     document.querySelector("body").style.background = "green"
     document.querySelector(".number").style.width = "30rem"
+    document.querySelector('.number').textContent = secretNumber;
+
+    if(score>highScore) {
+      highScore = score
+      document.querySelector(".highscore").textContent = highScore
+    }
   } else if (guess > secretNumber) {
     if(score>1) {
        document.querySelector('.message').textContent = 'to high';
@@ -36,4 +43,15 @@ const guess = Number(document.querySelector('.guess').value);
            document.querySelector('.score').textContent = 0;
     }
   }   
+})
+
+document.querySelector(".again").addEventListener("click", function() {
+let score = 20;
+secretNumber = Math.trunc(Math.random() * 20 + 1);
+ document.querySelector('.message').textContent = 'start guessing';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = "?"
+  document.querySelector(".guess").value =""
+    document.querySelector('body').style.background = 'black';
+    document.querySelector('.number').style.width = '15rem';
 })
